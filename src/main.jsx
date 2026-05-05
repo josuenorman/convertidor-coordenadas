94a980c1ae995bdaf9c0bb8cef4808eb35232f17
Norman Josué Garcia García <78883583+josuenorman@users.noreply.github.com>
2026-05-04 23:15:59 -0600

    Remove unused React entry file

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import App from './App.jsx'
-
-const rootElement = document.querySelector('#root')
-createRoot(rootElement).render(<App />)
